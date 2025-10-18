4096ea100f790651c584cab3f05b3a2d22122e37
MrKazzanek <161287375+MrKazzanek@users.noreply.github.com>
2025-10-18 20:13:09 +0200

    Update songs.js

--- songs.js
+++ songs.js
@@ -1,3 +1,4 @@
 const songs = [
-    { id: "lr_otherside_1", title: "Otherside", artist: "Lena Raine", src: "music/otherside.mp3", cover: "images/calm1.jpg" },
+    { id: "lr_otherside_1", title: "Otherside", artist: "Lena Raine", src: "music/otherside.mp3", cover: "images/otherside.jpg" },
 ];
+
