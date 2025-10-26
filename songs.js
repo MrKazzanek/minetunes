aa31fca502bfabdce2e410b9ae6bfa55a7f7480a
MrKazzanek <161287375+MrKazzanek@users.noreply.github.com>
2025-10-26 18:54:31 +0100

    Update songs.js

--- songs.js
+++ songs.js
@@ -2,8 +2,10 @@ const songs = [
     { id: "lena_raine-otherside", title: "Otherside", artist: "Lena Raine", src: "music/otherside.mp3", cover: "images/otherside.jpg" },
     { id: "lena_raine-pigstep", title: "Pigstep", artist: "Lena Raine", src: "music/pigstep.mp3", cover: "images/pigstep.jpg" },
     { id: "aaron_cherof-relic", title: "Relic", artist: "Aaron Cherof", src: "music/relic.mp3", cover: "images/relic.jpg" },
-    { id: "c418-thirteen", title: "Thirteen", artist: "C418", src: "music/Thirteen.mp3", cover: "images/thirteen.jpg" }
+    { id: "c418-thirteen", title: "Thirteen", artist: "C418", src: "music/Thirteen.mp3", cover: "images/thirteen.jpg" },
+    { id: "c418-cat", title: "Cat", artist: "C418", src: "music/Cat.mp3", cover: "images/thirteen.jpg" }
 ];
+
 
 
 
