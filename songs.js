3d22bd2644d618d355ba74be870dd7d632bc0659
MrKazzanek <161287375+MrKazzanek@users.noreply.github.com>
2025-10-26 19:31:54 +0100

    Update songs.js

--- songs.js
+++ songs.js
@@ -4,8 +4,10 @@ const songs = [
     { id: "aaron_cherof-relic", title: "Relic", artist: "Aaron Cherof", src: "music/relic.mp3", cover: "images/relic.jpg" },
     { id: "c418-thirteen", title: "Thirteen", artist: "C418", src: "music/Thirteen.mp3", cover: "images/thirteen.jpg" },
     { id: "c418-cat", title: "Cat", artist: "C418", src: "music/Cat.mp3", cover: "images/thirteen.jpg" },
-    { id: "c418-blocks", title: "Blocks", artist: "C418", src: "music/Blocks.mp3", cover: "images/miniaturka_r87ECi9RR_M.jpg" }
+    { id: "c418-blocks", title: "Blocks", artist: "C418", src: "music/Blocks.mp3", cover: "images/miniaturka_r87ECi9RR_M.jpg" },
+    { id: "c418-chirp", title: "Chirp", artist: "C418", src: "music/Chirp.mp3", cover: "images/miniaturka_r87ECi9RR_M.jpg" }
 ];
+
 
 
 
