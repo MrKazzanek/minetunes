02efea00afaeef5fec128f86e286324f236437e3
MrKazzanek <161287375+MrKazzanek@users.noreply.github.com>
2025-10-27 11:26:40 +0100

    Update songs.js

--- songs.js
+++ songs.js
@@ -2,7 +2,7 @@ const songs = [
     { id: "lena_raine-otherside", title: "Otherside", artist: "Lena Raine", src: "music/otherside.mp3", cover: "images/otherside.jpg", genre: ["Music Disc"], version: "1.18" },
     { id: "lena_raine-pigstep", title: "Pigstep", artist: "Lena Raine", src: "music/pigstep.mp3", cover: "images/pigstep.jpg", genre: ["Music Disc"], version: "1.16" },
     { id: "aaron_cherof-relic", title: "Relic", artist: "Aaron Cherof", src: "music/relic.mp3", cover: "images/relic.jpg", genre: ["Music Disc"], version: "1.20" },
-    { id: "c418-thirteen", title: "Thirteen", artist: "C418", src: "music/Thirteen.mp3", cover: "images/thirteen.jpg", genre: ["Music Disc"], version: "Alpha 1.0.14" },
+    { id: "c418-13", title: "13", artist: "C418", src: "music/Thirteen.mp3", cover: "images/thirteen.jpg", genre: ["Music Disc"], version: "Alpha 1.0.14" },
     { id: "c418-cat", title: "Cat", artist: "C418", src: "music/Cat.mp3", cover: "images/thirteen.jpg", genre: ["Music Disc"], version: "Alpha 1.0.14" },
     { id: "c418-blocks", title: "Blocks", artist: "C418", src: "music/Blocks.mp3", cover: "images/miniaturka_r87ECi9RR_M.jpg", genre: ["Music Disc"], version: "1.0.0" },
     { id: "c418-chirp", title: "Chirp", artist: "C418", src: "music/Chirp.mp3", cover: "images/miniaturka_r87ECi9RR_M.jpg", genre: ["Music Disc"], version: "1.0.0" },
@@ -18,6 +18,7 @@ const songs = [
     { id: "lena_raine-creator", title: "Creator", artist: "Lena Raine", src: "music/Creator.mp3", cover: "images/tricky.png", genre: ["Music Disc"], version: "1.21" },
     { id: "aaron_cherof-precipice", title: "Precipice", artist: "Aaron Cherof", src: "music/Precipice.mp3", cover: "images/tricky.png", genre: ["Music Disc"], version: "1.21" }
 ];
+
 
 
 
